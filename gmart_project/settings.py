47c17a8997fb01e7db77e6b59fc64137253ef061
Greg Ruiz <gregruiz@me.com>
2021-09-15 17:21:49 -0400

    troubleshooting

--- gmart_project/settings.py
+++ gmart_project/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'gmart_app/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
 # GR Change the default location of where media & image files are located
